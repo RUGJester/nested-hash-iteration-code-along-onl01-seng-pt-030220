118e512d8dcbd57feefdab7994948dc2ab711838
Sean Melton <seanlape@gmail.com>
2020-03-12 19:52:40 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -13,6 +13,19 @@ require 'pry'
   #     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
   #   }
   # }
+  puts remove_strawberry(contacts = {
+    "Jon Snow" => {
+      name: "Jon",
+      email: "jon_snow@thewall.we", 
+      favorite_icecream_flavors: ["chocolate", "vanilla"]
+    },
+    "Freddy Mercury" => {
+      name: "Freddy",
+      email: "freddy@mercury.com",
+      favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
+    }
+  })
+  
   
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
